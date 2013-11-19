43c8ba489fe73a0e1856a3d8623da72b32f40eea
Doug Emery <emeryr@upenn.edu>
2013-11-19 11:12:36 -0500

    add help and options to bash template

--- sh/template.sh
+++ sh/template.sh
@@ -1,8 +1,12 @@
 #!/bin/sh -ex
-
 # -ex:
 # bail on errors and show debugging stuff
+read -r -d '' HELP <<-'EOF'
+HELP TEXT HERE
 
+EOF
+
+################################################################################
 ### TEMPFILES
 # From:
 #   http://stackoverflow.com/questions/430078/shell-script-templates
@@ -13,11 +17,16 @@ trap "rm -f $tmp.?; exit 1" 0 1 2 3 13 15
 # then do
 #   ...real work that creates temp files $tmp.1, $tmp.2, ...
 
+################################################################################
 #### USAGE AND ERRORS
-cmd=`basename $0 .sh`
+cmd=`basename $0`
 
 usage() {
    echo "Usage: $cmd [ARGS..]"
+}
+
+print_help() {
+  echo "$HELP"
 }
 
 error() {
@@ -31,6 +40,7 @@ warning() {
 }
 
 
+################################################################################
 ### LOGGING
 logfile=${LOGFILE:LOG_${cmd}}.log
 
@@ -38,13 +48,35 @@ log() {
     echo "`date +%Y-%m-%dT%H:%M:%S` [$cmd] $1" >> $LOG
 }
 
+################################################################################
+### OPTIONS
+while getopts "h" opt; do
+  case $opt in
+    h)
+      usage 
+      print_help
+      exit 1
+      ;;
+    \?)
+      echo "ERROR Invalid option: -$OPTARG" >&2
+      echo ""
+      usage
+      exit 1
+      ;;
+  esac
+done
 
+shift $((OPTIND-1))
+
+
+################################################################################
 ### THESCRIPT
 
 
 
 
 
+################################################################################
 ### EXIT
 # http://stackoverflow.com/questions/430078/shell-script-templates
 rm -f $tmp.?
